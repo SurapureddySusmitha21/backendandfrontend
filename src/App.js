7597ce9ccb08d9ce876def36ce9d660f39a9d412
SurapureddySusmitha21 <surapureddysusmith@gmail.com>
2024-05-09 16:09:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect,useState} from "react"
+import "./App.css"
 
-function App() {
+const App=()=>{
+  const [user,setUser]=useState([])
+
+  const getUser=async ()=>{
+const response=await fetch ("/api/user")
+    const data=await response.json()
+    setUser(data)
+    //fetch("/api/user")
+   // .then(res => res.json())
+    //.then(json => console.log(json))
+  }
+
+  useEffect(()=>{
+    getUser()
+  },[])
+
+  console.log(user)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+    {user.map((each)=>{
+      return<>
+      <div className="container">
+<h1 className="">Name : {each.name}</h1>
+    <h1 className="">UserName :{each.username}</h1>
+    <h1 className="">Email :{each.email}</h1>
+      </div>
+      </>
+      
+    })}
+      
     </div>
-  );
+  )
 }
 
 export default App;
